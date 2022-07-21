249d1c786e828d259b9369834f047a3d96cc0831
andrii-balitskyi <10balian10@gmail.com>
2022-07-22 01:27:21 +0200

    some logging removed

--- utils/send-followup.js
+++ utils/send-followup.js
@@ -35,21 +35,6 @@ export const sendFollowup = ({ to, followupNumber, website }) => {
         },
       ],
     })
-    .then((info) => {
-      console.log("=============");
-      console.log(
-        JSON.stringify(
-          {
-            accepted: info.accepted,
-            rejected: info.rejected,
-            response: info.response,
-          },
-          null,
-          "  "
-        )
-      );
-      console.log("=============");
-    })
     .catch(console.error);
 };
 
