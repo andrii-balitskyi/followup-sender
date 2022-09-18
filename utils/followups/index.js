7cc1f35ca8b34bd10fa4f9441aeafc779b21b8bb
Andrii Balitskyi <10balian10@gmail.com>
2022-09-18 18:07:25 +0200

    add initial email

---
+++ utils/followups/index.js
@@ -0,0 +1,5 @@
+export * from "./get-initial-email.js";
+export * from "./get-1-followup.js";
+export * from "./get-2-followup.js";
+export * from "./get-3-followup.js";
+export * from "./get-4-followup.js";
